24f8064f32fedabb35f4ec532a62802807286e98
zdotdev <www.ragaszedrick25@gmail.com>
2025-01-01 13:55:07 +0800

    re:init

--- functions/models/recipe.js
+++
@@ -1,6 +0,0 @@
-const mongoose = require('mongoose')
-const recipeSchema = require('../schema/recipe.js')
-
-const recipeModel = mongoose.model('Recipe', recipeSchema)
-
-module.exports = recipeModel
