e9058d124fc9ac06dd49172406f61c2b4d961d65
zdotdev <www.ragaszedrick25@gmail.com>
2025-01-24 10:53:50 +0800

    Updated format dates

--- src/lib/Components/Election/ActiveElection.svelte
+++ src/lib/Components/Election/ActiveElection.svelte
@@ -1,14 +1,8 @@
 <script>
+    import formatDate from '$lib/dateFormatter';
     export let electionData = {};
     export let userChecker = null;
 
-    function formatDate(date) {
-        const d = new Date(date);
-        const month = String(d.getMonth() + 1).padStart(2, '0');
-        const day = String(d.getDate()).padStart(2, '0');
-        const year = d.getFullYear();
-        return `${month}-${day}-${year}`;
-    }
 </script>
 
 <div>
